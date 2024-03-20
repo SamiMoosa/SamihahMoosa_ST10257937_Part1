b0939be58f3aa649170dab7351f4e4c7ea349a7e
lab_services_student <lab_services_student@labVMH8OX>
2024-03-20 15:44:51 +0200

    Add project files.

---
+++ Controllers/UserContoller.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Test.Models;
+
+namespace Test.Controllers
+{
+    public class UserContoller : Controller
+    {
+
+        public Table_1 usrtbl= new Table_1();
+
+
+        [HttpPost]
+        public ActionResult About(Table_1 usrtbl)
+        {
+            var result = usrtbl.insert_User(usrtbl);
+            return RedirectToAction("Index", "Home");
+
+        }
+
+        [HttpGet]
+
+        public ActionResult About()
+        {
+            return View(usrtbl);
+        
+      
+        }
+    }
+}
